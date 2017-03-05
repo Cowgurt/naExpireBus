0ff75ff8b02a7be41042ba78f0b72733e5cef327
Graeme <graemedrucker@Graemes-MacBook-Pro.local>
2017-03-04 22:58:37 -0700

    Moved project directory

---
+++ NaExpireBusiness/app/src/main/java/com/capstone/naexpire/naexpirebusiness/MainActivity.java
@@ -0,0 +1,14 @@
+package com.capstone.naexpire.naexpirebusiness;
+
+import android.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
